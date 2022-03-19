cc8f54ebbcfb73e72dd33176a4a637f58e26aa90
Mrigank Tandon <sachitkapoor01@gmail.com>
2022-03-19 11:57:55 +0530

    Update UserBookApplication.java

--- src/main/java/com/example/UserBook/UserBookApplication.java
+++ src/main/java/com/example/UserBook/UserBookApplication.java
@@ -8,6 +8,8 @@ public class UserBookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserBookApplication.class, args);
+		
+		System.out.println("Mrignak");
 	}
 
 }
